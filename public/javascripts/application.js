5384c060fd41a15a6f174f471e2be7503845f853
Jesse Yuen <hello@jesseyuen.com>
2021-06-16 16:39:10 +0100

    Mobile filters added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,3 +8,25 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// Open and close filter panel on mobile
+const showFilterButton = document.querySelector('.js-show-filter')
+const showFilterButtonLabel = document.querySelector('.js-show-filter__label')
+const filterPanel = document.querySelector('.app-filter')
+const closeFilterButton = document.querySelector('.app-filter__close')
+
+showFilterButton.onclick = () => {
+  filterPanel.classList.add('js-isOpen');
+  // Change button label
+  showFilterButtonLabel.innerHTML = 'Close filters';
+  // shift the focus to the filter container
+  filterPanel.focus();
+}
+
+closeFilterButton.onclick = () => {
+  filterPanel.classList.remove('js-isOpen');
+  // Change button label
+  showFilterButtonLabel.innerHTML = 'Open filters';
+  // shift the focus back to where the user was originally
+  showFilterButton.focus();
+}
