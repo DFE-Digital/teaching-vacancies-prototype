2839ccc25c900165aa20e4e75426eb509e1b6059
Jesse Yuen <hello@jesseyuen.com>
2021-06-17 10:41:32 +0100

    Test for conditional js added and some links updated

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,24 +9,33 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
 
-// Open and close filter panel on mobile
-const showFilterButton = document.querySelector('.js-show-filter')
-const showFilterButtonLabel = document.querySelector('.js-show-filter__label')
-const filterPanel = document.querySelector('.app-filter')
-const closeFilterButton = document.querySelector('.app-filter__close')
-
-showFilterButton.onclick = () => {
-  filterPanel.classList.add('js-isOpen');
-  // Change button label
-  showFilterButtonLabel.innerHTML = 'Close filters';
-  // shift the focus to the filter container
-  filterPanel.focus();
+// Are we on the search results page?
+if (document.querySelector('.app-search-results') !== null) {
+  console.log("Search results page")
+  
+  const showFilterButton = document.querySelector('.js-show-filter')
+  const showFilterButtonLabel = document.querySelector('.js-show-filter__label')
+  const filterPanel = document.querySelector('.app-filter')
+  const closeFilterButton = document.querySelector('.app-filter__close')
+  
+  // Open the filter
+  showFilterButton.onclick = () => {
+    filterPanel.classList.add('js-isOpen');
+    // Change button label
+    showFilterButtonLabel.innerHTML = 'Close filters';
+    // Shift the focus to the filter container
+    filterPanel.focus();
+  }
+  
+  // Close the filter
+  closeFilterButton.onclick = () => {
+    filterPanel.classList.remove('js-isOpen');
+    // Change button label
+    showFilterButtonLabel.innerHTML = 'Open filters';
+    // Shift the focus back to where the user was originally
+    showFilterButton.focus();
+  }
 }
 
-closeFilterButton.onclick = () => {
-  filterPanel.classList.remove('js-isOpen');
-  // Change button label
-  showFilterButtonLabel.innerHTML = 'Open filters';
-  // shift the focus back to where the user was originally
-  showFilterButton.focus();
-}
+
+
