5495498ab3a0208f84d608af22905e1e38445f55
Adam Silver <adambsilver@gmail.com>
2022-09-12 16:58:13 +0100

    Tidy up school page

--- app/routes/schools.js
+++ app/routes/schools.js
@@ -1,10 +1,19 @@
 const schools = require('../data/orgs.json')
+const jobs = require('../data/jobs.json')
 
 module.exports = router => {
 
   router.get('/schools', (req, res) => {
     res.render('schools/index', {
       schools
+    })
+  })
+
+  router.get('/schools/show', (req, res) => {
+    let currentJobs = [jobs[0], jobs[1]]
+
+    res.render('schools/show', {
+      currentJobs
     })
   })
 
