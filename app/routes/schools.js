6fa6f61e9a2d841b861edc73a9e3c0c5efa46c0e
Adam Silver <adambsilver@gmail.com>
2022-10-24 12:23:12 +0100

    Trust profiles

--- app/routes/schools.js
+++ app/routes/schools.js
@@ -9,14 +9,35 @@ module.exports = router => {
   })
 
   router.get('/schools/:id', (req, res) => {
-    let organisation = req.session.data.organisations.find(organisation => organisation.id == req.params.id)
+    let organisations = req.session.data.organisations
+    let organisation = organisations.find(organisation => organisation.id == req.params.id)
 
     let jobs = req.session.data.jobs
 
     jobs = [jobs[0], jobs[1]]
 
+    // Check to see if the school is part of a trust
+    let parentOrganisation = organisations
+      .filter(o => o.schools)
+      .find(o => o.schools.find(s => s.id == organisation.id))
+
+    if(parentOrganisation) {
+      organisation.parentOrganisation = {
+        name: parentOrganisation.name,
+        id: parentOrganisation.id
+      }
+    }
+
     res.render('schools/show/index', {
       jobs,
+      organisation
+    })
+  })
+
+  router.get('/schools/:id/schools', (req, res) => {
+    let organisation = req.session.data.organisations.find(organisation => organisation.id == req.params.id)
+
+    res.render('schools/show/schools', {
       organisation
     })
   })
