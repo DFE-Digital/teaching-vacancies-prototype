5d52cb7314956a02cda17ea0f281215a9eb0ae0b
alexleedfe <115729523+alexleedfe@users.noreply.github.com>
2022-11-08 17:51:30 +0000

    initial commit, still a WIP just want to store the progress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,27 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+
+  if ($("#hide-school")[0]){
+
+      var selectEl = document.querySelector('#hide-school')
+      accessibleAutocomplete.enhanceSelectElement({
+        autoselect: true,
+        confirmOnBlur: true,
+        defaultValue: "",
+        minLength: 2,
+        selectElement: selectEl
+      })
+
+      var queryStringParameters = window.location.search
+      var previouslySubmitted = queryStringParameters.length > 0
+      if (previouslySubmitted) {
+        var submittedEl = document.querySelector('.submitted')
+        submittedEl.classList.remove('submitted--hidden')
+        var params = new URLSearchParams(document.location.search.split('?')[1])
+        document.querySelector('.submitted__hide-school').innerHTML = params.get('hide-school')
+      }
+
+  }
 })
---
+++ app/routes/profile-hide-profile.js
@@ -0,0 +1,88 @@
+const { DateTime } = require('luxon')
+const { v4: uuidv4 } = require('uuid');
+const _ = require('lodash');
+
+module.exports = router => {
+
+  router.get('/profile/hide-profile', (req, res) => {
+    let profile = req.session.user.profile
+
+    let hiddenPlace = _.get(req, 'session.data.profile.hiddenPlace')
+
+    res.render('profile/hide-profile/index', {
+      hiddenPlace
+    })
+  })
+
+  router.post('/profile/hide-profile', (req, res) => {
+
+    let hiddenPlace = {}
+    hiddenPlace.id = uuidv4()
+    hiddenPlace.hiddenPlace = req.body.profile.hiddenPlace
+
+    req.session.user.profile.hiddenPlaces[hiddenPlace.id] = hiddenPlace
+
+    res.redirect('/profile/hide-profile/review')
+  })
+
+  //edit
+
+
+  router.get('/profile/hide-profile/:id/edit', (req, res) => {
+
+    let id = req.params.id
+    let profile = req.session.user.profile
+
+    let item = profile.hideAll[id]
+
+    let hiddenPlace = item.hiddenPlace
+
+    res.render('profile/hide-profile/index', {
+      hiddenPlace
+    })
+
+  })
+
+  router.post('/profile/hide-profile/:id/edit', (req, res) => {
+
+    let id = req.params.id
+    var item = req.session.user.profile.hideAll[id]
+    item.hiddenPlace = req.body.profile.hiddenPlace
+
+    res.redirect(`/profile/hide-profile/review`)
+  })
+
+  //review
+
+  router.get('/profile/hide-profile/review', (req, res) => {
+
+    let profile = req.session.user.profile
+
+    res.render('profile/hide-profile/review', {
+      profile
+    })
+  })
+
+  router.get('/profile/hide-profile/delete', (req, res) => {
+
+    let id = req.params.id
+    let profile = req.session.user.profile
+    let hide = profile.hideAll[id]
+
+    res.render('profile/hide-profile/delete', {
+      hide
+    })
+  })
+
+  router.post('/profile/hide-profile/delete', (req, res) => {
+
+    let id = req.params.id
+    let profile = req.session.user.profile
+
+    delete profile.hideAll[id]
+
+    req.flash('success', 'School deleted')
+    res.redirect('/profile/hide-profile/review')
+  })
+
+}
