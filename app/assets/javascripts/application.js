6afc65198a4d009eac220026057ed6316f9e39b7
alexleedfe <115729523+alexleedfe@users.noreply.github.com>
2023-11-21 11:05:26 +0000

    initial concepts for ASR filters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -72,4 +72,31 @@ $(document).ready(function () {
   $("#filter-subjects legend").after('<div class="searchable-collection-component__search govuk-!-margin-bottom-2"><input aria-expanded="true" aria-label="Subject" aria-owns="subjects__listbox" class="govuk-input icon icon--left icon--search js-action" data-action="input->searchable-collection#input" data-searchable-collection-target="input" placeholder="Search" role="combobox"><div aria-live="assertive" class="govuk-visually-hidden collection-match" role="status"></div></div>');
 
 
+  const buttons = document.querySelectorAll('.app-c-option-select__button');
+
+  // Add a click event listener to each button
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      // Get the parent div of the clicked button
+      const parentDiv = button.closest('.app-c-option-select');
+
+      // Check if a certain class exists on the parent div
+      if (parentDiv && parentDiv.classList.contains('js-closed')) {
+        // If it exists, change the class of the parent div
+        parentDiv.classList.remove('js-closed');
+        parentDiv.classList.add('js-opened');
+      }else if (parentDiv && parentDiv.classList.contains('js-opened')) {
+        // If it exists, change the class of the parent div
+        parentDiv.classList.remove('js-opened');
+        parentDiv.classList.add('js-closed');
+      }
+
+      // Toggle the "aria-expanded" attribute on the button
+      const currentAriaExpanded = button.getAttribute('aria-expanded');
+      const newAriaExpanded = currentAriaExpanded === 'true' ? 'false' : 'true';
+      button.setAttribute('aria-expanded', newAriaExpanded);
+
+    });
+  });
+
 })
--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Find teaching jobs',
+  serviceName: 'Find school jobs',
 
   // Default port that prototype runs on
   port: '3000',
